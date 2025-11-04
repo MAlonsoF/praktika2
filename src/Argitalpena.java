7322d9659f32b807b3a8ff6989f1b4d54d9a72ec
anderRaya <anderraya10@gmail.com>
2025-11-04 13:23:53 +0100

    Update Argitalpena.java
    a

--- src/Argitalpena.java
+++ src/Argitalpena.java
@@ -1,61 +1,71 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+package labo2;
+
+
 
 public class Argitalpena {
 	private String idA;
 	private String izenburua;
-	private HashMap<String, Editorea> egileak;
-	private HashMap<String, Argitalpena> erlazionatuak;
+    private UnorderedDoubleLinkedList<Editorea> egileak;
+    private UnorderedDoubleLinkedList<Argitalpena> erlazionatuak;
 
 	public Argitalpena(String pIdA, String pIzenburua) {
 		this.idA = pIdA;
 		this.izenburua = pIzenburua;
-		this.egileak = new HashMap<String, Editorea>();
-		this.erlazionatuak = new HashMap<String, Argitalpena>();
+        this.egileak = new UnorderedDoubleLinkedList<>();
+        this.erlazionatuak = new UnorderedDoubleLinkedList<>();
 	}
 	 public String getIdA() { 
         return idA;
 	 }
 
 	 public String getIzenburua() { 
-        return izenburua;
+		return izenburua;
 	 }
 
 	 public void gehituEgilea(Editorea e) {
-	        if (!this.egileak.containsKey(e.getId())) {
-	            this.egileak.put(e.getId(), e);
+	        if (findEgilea(e.getId()) == null) {
+	            egileak.addToRear(e);
 	        }
 	 }
-	 public ArrayList<String> egileak() {
-		 ArrayList<String> lista = new ArrayList<String>();
-		 for (Editorea a : egileak.values()) {
-			 String id = a.getId();
-			 String idS = id.toString();
-			 lista.add(idS);
-		 }
-		 return lista;
-	 }
+	 public UnorderedDoubleLinkedList<String> egileak() {
+	        UnorderedDoubleLinkedList<String> lista = new UnorderedDoubleLinkedList<>();
+	        for (Editorea e : egileak) {
+	            lista.addToRear(e.getId());
+	        }
+	        return lista;
+	    }
 
 	 public void gehituArgitalpena(Argitalpena a) {
-        if ((a.idA.equals(""))||(a.izenburua.equals(""))) {
-            throw new NullPointerException("Argitalpena nulua da");
-        }
-		else if (!this.erlazionatuak.containsKey(a.idA)) {
-		        this.erlazionatuak.put(a.idA, a);
-		    }
-    }
+	        if (findArgitalpena(a.getIdA()) == null) {
+	            erlazionatuak.addToRear(a);
+	        }
+		} 
 
-     public ArrayList<String> aipamenak() {
-		 ArrayList<String> lista = new ArrayList<String>();
-		 for (Argitalpena a : erlazionatuak.values()) {
-			 String id = a.idA;
-			 String idS = id.toString();
-			 lista.add(idS);
-		 }
-		 return lista;
-	}
-    public void kenduEgilea(String idEgile) {
-        egileak.remove(idEgile);
+	public UnorderedDoubleLinkedList<String> aipamenak() {
+		UnorderedDoubleLinkedList<String> lista = new UnorderedDoubleLinkedList<>();
+	        for (Argitalpena a : erlazionatuak) {
+	            lista.addToRear(a.getIdA());
+	        }
+	        return lista;
+	    }
+	    
+     public void kenduEgilea(String idEgile) {
+         Editorea ezabatzeko = findEgilea(idEgile);
+         if (ezabatzeko != null) {
+             egileak.remove(ezabatzeko);
+         }
+     }
+    private Editorea findEgilea(String id) {
+        for (Editorea e : egileak) {
+            if (e.getId().equals(id)) return e;
+        }
+        return null;
+    }
+    private Argitalpena findArgitalpena(String id) {
+        for (Argitalpena a : erlazionatuak) {
+            if (a.getIdA().equals(id)) return a;
+        }
+        return null;
     }
 
 
