d37af6a5cdd01e08b8d4572e71ded1e21c20a3a5
MAlonsoF <marcoalonsofernandez@gmail.com>
2025-11-06 22:39:33 +0100

    Kasu proba de UnorderedDoubleLinkedList corregido

--- src/Testak/UnorderedDoubleLinkedListTest.java
+++ src/Testak/UnorderedDoubleLinkedListTest.java
@@ -1,5 +1,6 @@
 package Testak;
 
+import labo2.DoubleLinkedList;
 import labo2.UnorderedDoubleLinkedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,39 +8,56 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UnorderedDoubleLinkedListTest {
 
+    private UnorderedDoubleLinkedList<String> lista;
+    private UnorderedDoubleLinkedList<String> lista2;
     private UnorderedDoubleLinkedList<String> lista3;
 
     @BeforeEach
     public void setUp() {
-        lista3 = new UnorderedDoubleLinkedList<>();
+        lista = new UnorderedDoubleLinkedList<>();
+        UnorderedDoubleLinkedList<String> lista2 = new UnorderedDoubleLinkedList<>();// lista2 prellenada con "A", "B"
+        lista2.addToRear("A");
+        lista2.addToRear("B");
+        UnorderedDoubleLinkedList<String> lista3 = new UnorderedDoubleLinkedList<>();// lista3 prellenada con "A"
+        lista3.addToRear("A");
     }
 
     @Test
     public void testAddToFront() {
-        lista3.addToFront("A");
+        lista.addToFront("A");
+        assertEquals(1, lista.size());
+        assertEquals("A", lista.first());
         lista3.addToFront("B");
         assertEquals(2, lista3.size());
-        assertEquals("B", lista3.first());
-        assertEquals("A", lista3.last());
+        assertEquals("B", lista.first());
     }
 
     @Test
     public void testAddToRear() {
-        lista3.addToRear("A");
+        lista.addToRear("A");
+        assertEquals(1, lista.size());
+        assertEquals("A", lista.first());
         lista3.addToRear("B");
         assertEquals(2, lista3.size());
-        assertEquals("A", lista3.first());
         assertEquals("B", lista3.last());
     }
 
     @Test
     public void testAddAfter() {
-        lista3.addToRear("A");
-        lista3.addToRear("C");
+        lista.addToRear("A");
+        lista.addToRear("C");
+        lista.addAfter("B", "A");
+        assertEquals(3, lista.size());
+        assertEquals("A", lista.first());
+        assertTrue(lista.contains("B"));
+        assertEquals("C", lista.last());
+        lista2.addAfter("C", "B");
+        assertEquals(3, lista2.size());
+        assertEquals("C", lista2.last());
+        assertTrue(lista2.contains("C"));
         lista3.addAfter("B", "A");
-        assertEquals(3, lista3.size());
-        assertEquals("A", lista3.first());
+        assertEquals(2, lista3.size());
+        assertEquals("B", lista3.last());
         assertTrue(lista3.contains("B"));
-        assertEquals("C", lista3.last());
     }
 }
